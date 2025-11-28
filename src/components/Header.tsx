2f11b22fb96c0a13884552c68658a144f0fb612d
danipereiracodes <info.danipereira@gmail.com>
2025-11-28 09:10:31 +0100

    feat: change title name

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -87,7 +87,7 @@ const Header = () => {
             <Link to='/' className='flex items-center space-x-2'>
               <Code2 className='w-8 h-8 text-blue-600' />
               <span className='text-xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent'>
-                DPW
+                DANI PEREIRA
               </span>
             </Link>
 
--- src/components/HeaderLanding.tsx
+++ src/components/HeaderLanding.tsx
@@ -15,7 +15,7 @@ const HeaderLanding = () => {
             <Link to='/' className='flex items-center space-x-2'>
               <Code2 className='w-7 h-7 text-blue-600' />
               <span className='text-lg font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent'>
-                DPW
+                DANI PEREIRA
               </span>
             </Link>
 
@@ -48,4 +48,3 @@ const HeaderLanding = () => {
 };
 
 export default HeaderLanding;
-
