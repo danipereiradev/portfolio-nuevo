b00a383f48cbd14878eda6ce0237115e9350047e
danipereiracodes <info.danipereira@gmail.com>
2026-08-18 14:13:53 +0200

    fix: enhance button styles and refine layout in Team and TextImage components for improved consistency

--- src/components/TextImage.tsx
+++ src/components/TextImage.tsx
@@ -9,7 +9,7 @@ interface TextImageProps {
 export const TextImage = ({ label, title, paragraphs }: TextImageProps) => {
   return (
     <section className='mx-auto flex flex-col-reverse md:flex-row-reverse items-center md:h-[100vh] text-center md:text-start gap-8 py-8 px-4 md:py-0 md:px-8'>
-      <div className='md:w-1/2 flex flex-col items-center md:items-start gap-4 pl-8'>
+      <div className='md:w-1/2 flex flex-col items-center md:items-start gap-4 md:pl-8'>
         <span className='text-accent font-extrabold underline text-md rounded-2xl'>
           {label}
         </span>
